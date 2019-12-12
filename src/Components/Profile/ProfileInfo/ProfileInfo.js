2437f402e642da3c1a6e10e80a84d295adf1f69b
anton <antonapanovich1999@gmail.com>
2019-12-12 23:27:18 +0300

    add status component

--- src/Components/Profile/ProfileInfo/ProfileInfo.js
+++ src/Components/Profile/ProfileInfo/ProfileInfo.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import cl from './ProfileInfo.module.css'
 import Preloader from "../../../Common/Preloader/Preloader";
+import ProfileStatus from "../ProfileStatus/ProfileStatus";
 
 const ProfileInfo = (props) => {
     if(!props.profile){
@@ -8,9 +9,10 @@ const ProfileInfo = (props) => {
     }
     return (
         <div>
-            <img className={cl.img} src="http://ushki-ruchki.ru/uploads/images/s/a/s/sasha_gray_sweet_game.jpg"/>
+            {/*<img className={cl.img} src="http://ushki-ruchki.ru/uploads/images/s/a/s/sasha_gray_sweet_game.jpg"/>*/}
             <div>
                 <img src={props.profile.photos.small}/>
+                <ProfileStatus status={"hello"} />
                 <div>{props.profile.aboutMe}</div>
             </div>
         </div>
